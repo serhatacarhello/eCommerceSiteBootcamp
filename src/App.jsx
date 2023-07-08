05ce6bd0499fb16a447eadca17cd405f7b8e3909
serhatacarhello <serhat.acar.tr@gmail.com>
2023-07-08 22:13:51 +0300

    best seller product created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import './App.css'
 import Header from './components/header'
+import MainPage from './pages/MainPage'
+
 const  App=()=> {
 
   return (
     <>
      <Header/>
+     <MainPage/>
     </>
   )
 }
